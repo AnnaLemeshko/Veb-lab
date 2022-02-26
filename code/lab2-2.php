4f1cacdce9c8eff0bf6c5a593c6a61d8488c60f3
AnnaLemeshko <lemanna07@gmail.com>
2022-02-26 15:38:44 +0200

    Lab2-2

---
+++ code/lab2-2.php
@@ -0,0 +1,13 @@
+<?php
+    $a = 7;
+    echo $a;
+    echo "\n";
+
+    $b = 19.8;
+    echo $b;
+    echo (20 - 8);
+
+    $last_month = 1187.23;
+    $this_month = 1089.98;
+    echo ($last_month - $this_month);
+?>
